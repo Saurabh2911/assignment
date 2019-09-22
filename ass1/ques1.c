aea6cda2fe4a28219ce5d6bd5f9234a42dd91bac
Saurabh2911 <54993976+Saurabh2911@users.noreply.github.com>
2019-09-22 21:53:15 +0530

    Add files via upload

---
+++ ass1/ques1.c
@@ -0,0 +1,10 @@
+//TO CALCULATE AREA OF RECTANGLE
+#include<stdio.h>
+int main()
+{
+		printf("ENTER THE LENGTH & BREADTH OF REACTANGLE\n");
+		float l,b;
+		scanf("%f %f",&l,&b);
+		printf("\n THE AREA OF RECTANGLE IS :%f",l*b);
+		return 0;
+}
